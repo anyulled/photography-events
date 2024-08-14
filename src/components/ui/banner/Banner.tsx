ccff496a44f4ec89359b88644ac39554dcbb1ea6
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-08-14 11:13:09 +0200

    feat: improve calendar

--- src/components/ui/banner/Banner.tsx
+++ src/components/ui/banner/Banner.tsx
@@ -18,7 +18,7 @@ const bannerInfo = {
 
 export default function Example() {
   return (
-    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 my-0.5 sm:px-3.5 sm:before:flex-1 rounded">
       <div
         aria-hidden="true"
         className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
