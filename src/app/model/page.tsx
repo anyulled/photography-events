b5834b46719d14945be17e1a8e0127dae3b53fec
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-08-14 15:46:24 +0200

    fix: fix build error

--- src/app/model/page.tsx
+++ src/app/model/page.tsx
@@ -7,7 +7,6 @@ export const metadata: Metadata = {
 };
 
 export default function Models() {
-  console.log(generatePersonData());
   return (
     <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight capitalize">
