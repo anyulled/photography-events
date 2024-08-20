d5e4db7b0acdf441c942042b9299649ed7308dcd
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-08-20 10:06:44 +0200

    feat: google Oauth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth";
 import Instagram from "next-auth/providers/instagram";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Instagram],
+  providers: [
+    Instagram,
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 });
