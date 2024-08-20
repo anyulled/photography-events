e43f8923abf6c2c1012fb765ada1ffa0793f1ff9
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-08-20 10:10:02 +0200

    feat: login with Google

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,50 @@
 import { signIn } from "../../../auth";
+import { Metadata } from "next";
+import { title } from "@/components/constants"; /* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/no-img-element */
+
+export const metadata: Metadata = {
+  title: title + " Login",
+};
 
 export default function Login() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <form
+        className={"pt-1"}
         action={async () => {
           "use server";
-          await signIn("instagram");
+          await signIn("Google");
         }}
       >
-        <button type="submit">Sign in with Instagram</button>
+        <button type="submit" className={"flex hover:bg-gray-200 p-5"}>
+          <img
+            src={
+              "https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg"
+            }
+            width={24}
+            height={24}
+            alt={"Google"}
+          />{" "}
+          <span className={"ml-2"}>Sign in with Google</span>
+        </button>
+      </form>
+      <form
+        className={"pb-1"}
+        action={async () => {
+          "use server";
+          await signIn("Instagram");
+        }}
+      >
+        <button type="submit" className={"flex hover:bg-gray-200 p-5"}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"
+            alt="Instagram"
+            width={24}
+            height={24}
+          />
+          <span className={"ml-2"}>Sign in with Instagram</span>
+        </button>
       </form>
     </main>
   );
