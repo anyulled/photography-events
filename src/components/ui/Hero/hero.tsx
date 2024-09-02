2b34e51d0d85d32e6a8111bb41a43170a507aa62
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-09-02 16:38:59 +0200

    chore: fix sonar errors

--- src/components/ui/Hero/hero.tsx
+++ src/components/ui/Hero/hero.tsx
@@ -1,10 +1,10 @@
 export function Hero({
   description,
   title,
-}: {
+}: Readonly<{
   title: string;
   description: string;
-}) {
+}>) {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-r from-teal-500 to-orange-500">
       <div className="container px-4 md:px-6">
